dfde604577d5c9c35052d5ddcab9e113cfd1fc49
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-05-16 22:05:23 +0530

    create react app

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='user'>Welcome User</div>
+        <button>Log Out</button>
+    </div>
+  )
+}
+
+export default Navbar
